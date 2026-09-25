6fd5eb9ec02aa41ea24b25ee103ddeef796a1d48
DatNguyenSE <datnt05se@gmail.com>
2026-09-25 18:33:58 +0700

    Remove temporary Python maintenance scripts

--- check.py
+++
@@ -1,9 +0,0 @@
-﻿import sys
-
-with open(r'd:\ADMIN\Documents\FreeLancer\QuangAnhMT.com\QuangAnhMT.com\App_Code\db.designer.cs', 'r', encoding='utf-8') as f:
-    cs = f.read()
-
-if 'PhuCap_RnD' not in cs:
-    print('Missing PhuCap_RnD in designer!')
-else:
-    print('PhuCap_RnD exists in designer!')
